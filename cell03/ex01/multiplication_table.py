e2653563a4d8b831ddb2ac311bc5346f337dd401
bsrxwt <sorawit281249@gmail.com>
2025-09-15 17:57:53 +0700

    submit

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+i=0
+while i < 10:
+    mult = i*num
+    print(f"{i} x {num} = {mult}")
+    i+=1
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,6 @@
+st = "STOP"
+user = input("What you gotta say? : ")
+while user != st:
+    user = input("I got that! Anything else? : ")
+
+print("I got that!")
